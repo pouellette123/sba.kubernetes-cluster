2e90dcc55153fdbd44754d18aaee773ab1971126
Paul Ouellette <pouellette123es@gmail.com>
2021-01-19 11:22:06 -0500

    added variable LOG_FILE to kubernetes cluster info script

--- kubernetes-cluster-information.sh
+++ kubernetes-cluster-information.sh
@@ -2,58 +2,59 @@
 # Script to output information on a kubernetes cluster to the file:
 # ./kuberenetes-cluster-information-output
 #
-echo -n "Paul Ouellette SBA Kubernetes Cluster          " > ./kuberenetes-cluster-information-output
-echo `date` >> ./kuberenetes-cluster-information-output
-echo "" >> ./kuberenetes-cluster-information-output
-echo "kubernetes-cluster-information.sh script output:" >> ./kuberenetes-cluster-information-output
-echo "" >> ./kuberenetes-cluster-information-output
+LOG_FILE="./kuberenetes-cluster-information-output"
+echo -n "Paul Ouellette SBA Kubernetes Cluster          " > $LOG_FILE
+echo `date` >> $LOG_FILE
+echo "" >> $LOG_FILE
+echo "kubernetes-cluster-information.sh script output:" >> $LOG_FILE
+echo "" >> $LOG_FILE
 
-echo "This script will run commands to get information on the state " >> ./kuberenetes-cluster-information-output
-echo "of the kubernetes cluster running for the sba-kubernetes-cluster." >> ./kuberenetes-cluster-information-output
-echo "" >> ./kuberenetes-cluster-information-output
+echo "This script will run commands to get information on the state " >> $LOG_FILE
+echo "of the kubernetes cluster running for the sba-kubernetes-cluster." >> $LOG_FILE
+echo "" >> $LOG_FILE
 
-echo "The cluster information is provided by kubectl cluster-info." >> ./kuberenetes-cluster-information-output
-kubectl cluster-info >> ./kuberenetes-cluster-information-output
-echo "" >> ./kuberenetes-cluster-information-output
+echo "The cluster information is provided by kubectl cluster-info." >> $LOG_FILE
+kubectl cluster-info >> $LOG_FILE
+echo "" >> $LOG_FILE
 
-echo "The following minikube IP address is used in the browswer to bring up the app." >> ./kuberenetes-cluster-information-output
-echo "It is the node's IP address, the entry point into the cluster." >> ./kuberenetes-cluster-information-output
-echo -n "minikube IP address is: " >> ./kuberenetes-cluster-information-output
-minikube ip >> ./kuberenetes-cluster-information-output
+echo "The following minikube IP address is used in the browswer to bring up the app." >> $LOG_FILE
+echo "It is the node's IP address, the entry point into the cluster." >> $LOG_FILE
+echo -n "minikube IP address is: " >> $LOG_FILE
+minikube ip >> $LOG_FILE
 
-echo "" >> ./kuberenetes-cluster-information-output
-echo "The cluster nodes information short form, retrieved by kubectl get nodes, is:" >> ./kuberenetes-cluster-information-output
-kubectl get nodes >> ./kuberenetes-cluster-information-output
+echo "" >> $LOG_FILE
+echo "The cluster nodes information short form, retrieved by kubectl get nodes, is:" >> $LOG_FILE
+kubectl get nodes >> $LOG_FILE
 
-echo "" >> ./kuberenetes-cluster-information-output
-echo "The pyflaskapp-service Node PORT number, mapped 8080 internally to 32001 externally," >> ./kuberenetes-cluster-information-output
-echo "is used after the IP address listed above in the browswer to bring up the app." >> ./kuberenetes-cluster-information-output
-echo "The cluster services information short form, retrieved by kubectl get services, is:" >> ./kuberenetes-cluster-information-output
-kubectl get services >> ./kuberenetes-cluster-information-output
+echo "" >> $LOG_FILE
+echo "The pyflaskapp-service Node PORT number, mapped 8080 internally to 32001 externally," >> $LOG_FILE
+echo "is used after the IP address listed above in the browswer to bring up the app." >> $LOG_FILE
+echo "The cluster services information short form, retrieved by kubectl get services, is:" >> $LOG_FILE
+kubectl get services >> $LOG_FILE
 
-echo "" >> ./kuberenetes-cluster-information-output
-echo "The cluster deployments information short form, retrieved by kubectl get deployments, is:" >> ./kuberenetes-cluster-information-output
-kubectl get deployments >> ./kuberenetes-cluster-information-output
+echo "" >> $LOG_FILE
+echo "The cluster deployments information short form, retrieved by kubectl get deployments, is:" >> $LOG_FILE
+kubectl get deployments >> $LOG_FILE
 
-echo "" >> ./kuberenetes-cluster-information-output
-echo "The cluster pods information short form, retrieved by kubectl get pods, is:" >> ./kuberenetes-cluster-information-output
-kubectl get pods >> ./kuberenetes-cluster-information-output
+echo "" >> $LOG_FILE
+echo "The cluster pods information short form, retrieved by kubectl get pods, is:" >> $LOG_FILE
+kubectl get pods >> $LOG_FILE
 
-echo "" >> ./kuberenetes-cluster-information-output
-echo "The cluster nodes information long form, retrieved by kubectl describe nodes, is:" >> ./kuberenetes-cluster-information-output
-kubectl describe nodes >> ./kuberenetes-cluster-information-output
+echo "" >> $LOG_FILE
+echo "The cluster nodes information long form, retrieved by kubectl describe nodes, is:" >> $LOG_FILE
+kubectl describe nodes >> $LOG_FILE
 
-echo "" >> ./kuberenetes-cluster-information-output
-echo "The pyflaskapp-service Node PORT number, mapped 8080 internally to 32001 externally," >> ./kuberenetes-cluster-information-output
-echo "is used after the IP address listed above in the browswer to bring up the app." >> ./kuberenetes-cluster-information-output
-echo "The cluster services information long form, retrieved by kubectl describe services, is:" >> ./kuberenetes-cluster-information-output
-kubectl describe services >> ./kuberenetes-cluster-information-output
+echo "" >> $LOG_FILE
+echo "The pyflaskapp-service Node PORT number, mapped 8080 internally to 32001 externally," >> $LOG_FILE
+echo "is used after the IP address listed above in the browswer to bring up the app." >> $LOG_FILE
+echo "The cluster services information long form, retrieved by kubectl describe services, is:" >> $LOG_FILE
+kubectl describe services >> $LOG_FILE
 
-echo "" >> ./kuberenetes-cluster-information-output
-echo "The cluster deployments information long form, retrieved by kubectl describe deployments, is:" >> ./kuberenetes-cluster-information-output
-kubectl describe deployments >> ./kuberenetes-cluster-information-output
+echo "" >> $LOG_FILE
+echo "The cluster deployments information long form, retrieved by kubectl describe deployments, is:" >> $LOG_FILE
+kubectl describe deployments >> $LOG_FILE
 
-echo "" >> ./kuberenetes-cluster-information-output
-echo "The cluster pods information long form, retrieved by kubectl dsescribe pods, is:" >> ./kuberenetes-cluster-information-output
-kubectl describe pods >> ./kuberenetes-cluster-information-output
+echo "" >> $LOG_FILE
+echo "The cluster pods information long form, retrieved by kubectl dsescribe pods, is:" >> $LOG_FILE
+kubectl describe pods >> $LOG_FILE
 
